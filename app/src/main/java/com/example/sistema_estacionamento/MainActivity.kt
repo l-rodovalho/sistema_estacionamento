b48653cb90629c87f5c88817cf44d94cbb21f4e2
TheMiranha <lxbrmiranda@gmail.com>
2025-01-07 00:58:31 -0300

    SignIn Setup

--- app/src/main/java/com/example/sistema_estacionamento/MainActivity.kt
+++ app/src/main/java/com/example/sistema_estacionamento/MainActivity.kt
@@ -6,28 +6,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sistema_estacionamento.views.SignInView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        var instance: MainActivity? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val intent = Intent(this, LoginActivity::class.java)
+        instance = this;
+
+        val intent = Intent(this, SignInView::class.java)
         startActivity(intent)
-
-        // Aplicar padding para barras do sistema
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-        //    insets
-        //}
-
-        // Configurar clique no botão "Veículos"
-        //val btnVeiculos = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.btnVeiculos)
-        //btnVeiculos.setOnClickListener {
-            // Abrir a tela da lista de carros
-        //    val intent = Intent(this, CarsListActivity::class.java)
-        //    startActivity(intent)
-        //}
     }
 }
---
+++ app/src/main/java/com/example/sistema_estacionamento/views/SignUpView.kt
@@ -0,0 +1,4 @@
+package com.example.sistema_estacionamento.views
+
+class SignUpView {
+}
